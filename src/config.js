8801a8a5cf1508fbc70569ac699db615f243708a
RyanJKS <rjhelan@gmail.com>
2022-11-26 20:08:53 +0000

    new backend

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: "https://jhelan-backend.herokuapp.com/"
+    baseURL: "https://portfolio-backend-jet.vercel.app/"
 })
